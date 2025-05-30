61f2eef002a803a7e8ba25894c28ed25d43aee26
Marcos <marcosnicolau2003@gmailcom>
2025-05-30 00:37:24 -0300

    feat: rm_all tables and data script

--- sql/rm_all.sql
+++ sql/rm_all.sql
@@ -1,47 +1,47 @@
 USE [GD1C2025]
 GO
 
-DELETE FROM MAUV.Sucursal
-DELETE FROM MAUV.Pedido
-DELETE FROM MAUV.Cliente
-DELETE FROM MAUV.Factura
-DELETE FROM MAUV.Detalle_Factura
-DELETE FROM MAUV.Sillon
-DELETE FROM MAUV.Envio
-DELETE FROM MAUV.Cancelacion_Pedido
-DELETE FROM MAUV.Compra
-DELETE FROM MAUV.Detalle_Compra
-DELETE FROM MAUV.Proveedor
-DELETE FROM MAUV.Detalle_Pedido
-DELETE FROM MAUV.Sillon_Medida
-DELETE FROM MAUV.SillonXMaterial
-DELETE FROM MAUV.Sillon_Modelo
-DELETE FROM MAUV.Tela
-DELETE FROM MAUV.Madera
-DELETE FROM MAUV.Relleno
-DELETE FROM MAUV.Material
-go
+DELETE FROM [MAUV].[Sucursal]
+DELETE FROM [MAUV].[Pedido]
+DELETE FROM [MAUV].[Cliente]
+DELETE FROM [MAUV].[Factura]
+DELETE FROM [MAUV].[Detalle_Factura]
+DELETE FROM [MAUV].[Sillon]
+DELETE FROM [MAUV].[Envio]
+DELETE FROM [MAUV].[Cancelacion_Pedido]
+DELETE FROM [MAUV].[Compra]
+DELETE FROM [MAUV].[Detalle_Compra]
+DELETE FROM [MAUV].[Proveedor]
+DELETE FROM [MAUV].[Detalle_Pedido]
+DELETE FROM [MAUV].[Sillon_Medida]
+DELETE FROM [MAUV].[SillonXMaterial]
+DELETE FROM [MAUV].[Sillon_Modelo]
+DELETE FROM [MAUV].[Tela]
+DELETE FROM [MAUV].[Madera]
+DELETE FROM [MAUV].[Relleno]
+DELETE FROM [MAUV].[Material]
+GO
 
-DROP FROM MAUV.Sucursal
-DROP FROM MAUV.Pedido
-DROP FROM MAUV.Cliente
-DROP FROM MAUV.Factura
-DROP FROM MAUV.Detalle_Factura
-DROP FROM MAUV.Sillon
-DROP FROM MAUV.Envio
-DROP FROM MAUV.Cancelacion_Pedido
-DROP FROM MAUV.Compra
-DROP FROM MAUV.Detalle_Compra
-DROP FROM MAUV.Proveedor
-DROP FROM MAUV.Detalle_Pedido
-DROP FROM MAUV.Sillon_Medida
-DROP FROM MAUV.SillonXMaterial
-DROP FROM MAUV.Sillon_Modelo
-DROP FROM MAUV.Tela
-DROP FROM MAUV.Madera
-DROP FROM MAUV.Relleno
-DROP FROM MAUV.Material
-go
+DROP TABLE [MAUV].[Sucursal]
+DROP TABLE [MAUV].[Pedido]
+DROP TABLE [MAUV].[Cliente]
+DROP TABLE [MAUV].[Factura]
+DROP TABLE [MAUV].[Detalle_Factura]
+DROP TABLE [MAUV].[Sillon]
+DROP TABLE [MAUV].[Envio]
+DROP TABLE [MAUV].[Cancelacion_Pedido]
+DROP TABLE [MAUV].[Compra]
+DROP TABLE [MAUV].[Detalle_Compra]
+DROP TABLE [MAUV].[Proveedor]
+DROP TABLE [MAUV].[Detalle_Pedido]
+DROP TABLE [MAUV].[Sillon_Medida]
+DROP TABLE [MAUV].[SillonXMaterial]
+DROP TABLE [MAUV].[Sillon_Modelo]
+DROP TABLE [MAUV].[Tela]
+DROP TABLE [MAUV].[Madera]
+DROP TABLE [MAUV].[Relleno]
+DROP TABLE [MAUV].[Material]
+GO
 
-DROP SCHEMA MAUV
+DROP SCHEMA [MAUV]
 GO
