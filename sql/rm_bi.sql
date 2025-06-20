1de763bfe57d51a43dfd54f397c70a1ad3f3c06b
nicolau <marcosnicolau2003@gmail.com>
2025-06-20 14:59:53 -0300

    fix drop view in rm_bi

--- sql/rm_bi.sql
+++ sql/rm_bi.sql
@@ -1,6 +1,18 @@
 USE [GD1C2025]
 GO
 
+DROP VIEW MAUV.BI_Vista_Ganancias_Mensuales;
+DROP VIEW MAUV.BI_Vista_Promedio_Factura_Mensual_Provincia;
+DROP VIEW MAUV.BI_Vista_Volumen_Pedidos;
+DROP VIEW MAUV.BI_Vista_Rendimiento_Modelos;
+DROP VIEW MAUV.BI_Vista_Volumen_Pedidos;
+DROP VIEW MAUV.BI_Vista_Conversion_Pedidos;
+DROP VIEW MAUV.BI_Vista_Promedio_Compras_Mensual;
+DROP VIEW MAUV.BI_Vista_Compras_Tipo_Material;
+DROP VIEW MAUV.BI_Vista_Porcentaje_Cumplimiento_Envios;
+DROP VIEW MAUV.BI_Vista_Top_Localidades_Costo_Envio;
+GO
+
 DELETE FROM [MAUV].[BI_Tiempo]
 DELETE FROM [MAUV].[BI_Ubicacion]
 DELETE FROM [MAUV].[BI_Rango_Etario_Cliente]
